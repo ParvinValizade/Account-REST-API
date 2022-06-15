3dbe8858d40ba6b18a3c1f86b092abd2f15417f2
ParvinValizade <parvin.valizade@mail.ru>
2022-06-15 21:32:47 +0400

    Initial commit

---
+++ src/main/java/com/company/account/service/AccountService.java
@@ -0,0 +1,46 @@
+package com.company.account.service;
+
+import com.company.account.dto.AccountDto;
+import com.company.account.dto.AccountDtoConverter;
+import com.company.account.dto.CreateAccountRequest;
+import com.company.account.model.Account;
+import com.company.account.model.Customer;
+import com.company.account.model.Transaction;
+import com.company.account.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final CustomerService customerService;
+    private final AccountDtoConverter converter;
+
+    public AccountService(AccountRepository accountRepository,
+                          CustomerService customerService,
+                          AccountDtoConverter converter) {
+        this.accountRepository = accountRepository;
+        this.customerService = customerService;
+        this.converter = converter;
+    }
+
+    public AccountDto createAccount(CreateAccountRequest createAccountRequest){
+        Customer customer = customerService.findCustomerById(createAccountRequest.getCustomerId());
+
+        Account account = new Account(
+                customer,
+                createAccountRequest.getInitialCredit(),
+                LocalDateTime.now());
+
+        if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0){
+            Transaction transaction = new Transaction(createAccountRequest.getInitialCredit(),account);
+
+            account.getTransaction().add(transaction);
+        }
+
+        return converter.convert(accountRepository.save(account));
+    }
+}
---
+++ src/main/java/com/company/account/service/CustomerService.java
@@ -0,0 +1,30 @@
+package com.company.account.service;
+
+import com.company.account.dto.CustomerDto;
+import com.company.account.dto.CustomerDtoConverter;
+import com.company.account.exception.CustomerNotFoundException;
+import com.company.account.model.Customer;
+import com.company.account.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter converter;
+
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter converter) {
+        this.customerRepository = customerRepository;
+        this.converter = converter;
+    }
+
+    protected Customer findCustomerById(String id){
+        return customerRepository.findById(id)
+                .orElseThrow(
+                        ()-> new CustomerNotFoundException("Customer could not find by id: " + id));
+    }
+
+    public CustomerDto getCustomerById(String customerId) {
+        return converter.convertToCustomerDto(findCustomerById(customerId));
+    }
+}
