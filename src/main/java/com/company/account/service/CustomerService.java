1e6df7e1ea7bb9408d128bf8f33fc99e6b219b35
ParvinValizade <parvin.valizade@mail.ru>
2022-06-19 14:20:02 +0400

    Integration Test

--- src/main/java/com/company/account/service/CustomerService.java
+++ src/main/java/com/company/account/service/CustomerService.java
@@ -7,6 +7,9 @@ import com.company.account.model.Customer;
 import com.company.account.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomerService {
 
@@ -26,5 +29,13 @@ public class CustomerService {
 
     public CustomerDto getCustomerById(String customerId) {
         return converter.convertToCustomerDto(findCustomerById(customerId));
+    }
+
+    public List<CustomerDto> getAllCustomer() {
+
+        return customerRepository.findAll()
+                .stream()
+                .map(converter::convertToCustomerDto)
+                .collect(Collectors.toList());
     }
 }
